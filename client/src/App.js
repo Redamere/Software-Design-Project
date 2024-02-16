97daa53f78a8e57f96507d25946398ce10e505e7
andrewdieu <andrewdieu2011@gmail.com>
2024-02-15 18:52:39 -0600

    Setting up React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from './components/loginPage/login';
+// import SignupPage from './components/signupPage/signup';
+
+const App = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default App;
