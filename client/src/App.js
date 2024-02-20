dcfab9457e605fe7f6d7b69d969dcec11e317b4f
Ivy Doan <huynhdoan14072002@gmail.com>
2024-02-20 15:43:05 -0600

    Login/Signup
    Login Page

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import LoginPage from './components/loginPage/login';
+import SignupPage from './components/signupPage/signup';
+
+const App = () => {
+  return (
+    <div>
+      <SignupPage/>
+    </div>
+  );
+};
+
+export default App;
