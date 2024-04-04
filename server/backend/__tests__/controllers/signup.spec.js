646c2017c736a5ef04df2e3b6148862dbed73cfb
Ivy Doan <huynhdoan14072002@gmail.com>
2024-04-03 23:43:15 -0500

    test

--- server/backend/__tests__/controllers/signup.spec.js
+++ server/backend/__tests__/controllers/signup.spec.js
@@ -1,121 +1,90 @@
-const request = require('supertest');
-const AuthController = require('../../controller/signup');
+const UserController = require('../../controller/signup');
 const User = require('../../models/user');
-const bcrypt = require('bcrypt');
-const { response } = require('express');
 
-jest.mock('bcrypt', () => ({
-  hash: jest.fn((password, salt) => Promise.resolve('hashedPassword'))
-}));
+jest.mock('../../models/user');
 
-
-describe('AuthController', () => {
+describe('signupUser', () => {
   beforeEach(() => {
     jest.clearAllMocks(); // Clear all mocked functions before each test
   });
 
-  it('should return 400 if username and password are not provided', async () => {
-    const req = { body: {} };
+  it('should create a new user with status 201', async () => {
+    // Mock data for request body
+    const newUser = {
+      username: 'john_doe',
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+    const createdUser = {
+      _id: 'userId',
+      ...newUser
+    };
+
+    User.create.mockResolvedValue(createdUser);
+    const req = { body: newUser };
     const res = {
-      status: jest.fn(() => res),
+      status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
 
-    await AuthController(req, res);
+    await UserController.signupUser(req, res);
+    expect(User.create).toHaveBeenCalledWith(newUser);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith({ user: createdUser });
+  });
 
+  it('should return status 400 if there is an error during user creation', async () => {
+    const req = { body: { username: 'john_doe', password: 'password123' } };
+
+    const errorMessage = 'Password and confirmPassword do not match';
+
+    User.create.mockRejectedValue(new Error(errorMessage));
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await UserController.signupUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Username and password are required' });
+    expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
   });
-
-  it('should return 400 if password does not meet strength requirements', async () => {
-    const req = { body: { username: 'testuser', password: 'weak' } };
-    const res = {
-      status: jest.fn(() => res),
-      json: jest.fn(),
-    };
-
-    await AuthController(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Password must be at least 8 characters long and contain at least one letter and one digit' });
-  });
-
-  it('should return 400 if user already exists', async () => {
-    const req = { body: { username: 'existinguser', password: 'StrongPassword123' } };
-    User.findOne = jest.fn(() => true); // Mocking User.findOne to return true (user exists)
-    const res = {
-      status: jest.fn(() => res),
-      json: jest.fn(),
-    };
-
-    await AuthController(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ message: 'User already exists' });
-  });
-
-  it('should return 500 if an internal server error occurs', async () => {
-    // Mock request object
-    const req = { body: { username: 'newuser', password: 'StrongPassword123' } };
-
-    // Mock User.findOne to throw an error
-    User.findOne = jest.fn(() => { throw new Error('Internal Server Error'); });
-
-    
-    // Mock response object
-    const res = {
-      status: jest.fn(() => res),
-      json: jest.fn(),
-    };
-
-    await AuthController(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Internal Server Error' });
-    User.findOne.mockRestore();
-  });
-  
-
-  describe('User Model', () => {
-    it('should return 500 if an internal server error occurs', async () => {
-        // Mock request object
-        const req = { body: { username: 'newuser', password: 'StrongPassword123' } };
-      
-        // Mock User.findOne to throw an error
-        try {
-          // Mock User.findOne to throw an error
-          User.findOne = jest.fn(() => { throw new Error('Internal Server Error'); });
-          const res = {
-            status: jest.fn(() => res),
-            json: jest.fn(),
-          };
-        
-          await AuthController(req, res);
-          expect(res.status).toHaveBeenCalledWith(500);
-          expect(res.json).toHaveBeenCalledWith({ message: 'Internal Server Error' });
-        } finally {
-          // Always restore the original implementation of User.findOne
-          User.findOne.mockRestore();
-        }
-      });
-  });
-  
-  it('should create new user and return 201 if user is registered successfully', async () => {
-    const req = { body: { username: 'newuser', password: 'StrongPassword123' } };
-    const res = {
-      status: jest.fn().mockReturnThis(), 
-      json: jest.fn() 
-    };
-
-    await AuthController(req, res); // Make sure to await the completion of AuthController
-
-    // // Assertions
-    expect(bcrypt.hash).toHaveBeenCalledWith('StrongPassword123', 10); // Check if bcrypt.hash is called with the correct arguments
-    expect(User.create).toHaveBeenCalledWith({ username: 'newuser', password: 'hashedPassword' }); // Check if User.create is called with the correct arguments
-    expect(res.status).toHaveBeenCalledWith(201); // Check if response status is 201
-    expect(res.json).toHaveBeenCalledWith({ message: 'User registered successfully' }); // Check if response JSON is correct
-  });
-  
-
 });
 
+describe('loginUser', () => {
+  it('should return status 200 if login is successful', async () => {
+    const req = { body: { username: 'fleet', password: 'password' } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Call loginUser function
+    await UserController.loginUser(req, res);
+
+    // Check if response status is 200
+    expect(res.status).toHaveBeenCalledWith(200);
+
+    // Check if response JSON includes success message
+    expect(res.json).toHaveBeenCalledWith({ message: 'Login Successful' });
+  });
+
+  it('should return status 401 if login fails due to incorrect credentials', async () => {
+    // Mock request object with invalid credentials
+    const req = { body: { username: 'invalid_user', password: 'invalid_password' } };
+
+    // Mock response object
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Call loginUser function
+    await UserController.loginUser(req, res);
+
+    // Check if response status is 401
+    expect(res.status).toHaveBeenCalledWith(401);
+
+    // Check if response JSON includes error message
+    expect(res.json).toHaveBeenCalledWith({ message: 'Username or password is incorrect' });
+  });
+});
