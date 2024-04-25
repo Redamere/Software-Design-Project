950efb82b84547a02731cd8ec876e3d6a0410ad6
Ivy Doan <huynhdoan14072002@gmail.com>
2024-04-24 20:25:30 -0500

    home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,24 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import backgroundImage from "../Assets/oil_picture.jpg";
 
 const Home = () => {
     return (
-        <>
+        <div className="home-container">
             <div className="home">
-                <h2>Home</h2>
+                <div className="home-content">
+                    <h1>Welcome to Fleet</h1>
+                    <p>A professional team dedicated to precise fuel rate predictions tailored to your needs.</p>
+                    <Link to="/form"><button>Get Started</button></Link> {/* Wrap button with Link */}
+                </div>
             </div>
-        </>
-    )
-}
+            <div className="home-body">
+                <h2>Our Services</h2>
+                <p>Learn more about how we can help optimize your fleet's fuel consumption.</p>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
