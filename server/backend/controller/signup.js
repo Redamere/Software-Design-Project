0b7e0a0b21ae9f2b5c44a2df4b944e28ca7a0a91
Andrew Dieu <andrewdieu2011@gmail.com>
2024-04-25 01:15:20 -0500

    checks if user exists

--- server/backend/controller/signup.js
+++ server/backend/controller/signup.js
@@ -1,24 +1,28 @@
-const User = require('../models/user')
-
+const User = require('../models/user');
 
 const signupUser = async (req, res) => {
-  const { username, password} = req.body;
+  const { username, password } = req.body;
   try {
     // Validate request body
     if (!username || !password) {
-      return res.status(400).json({ message: 'Username, password, and confirmPassword are required' });
+      return res.status(400).json({ message: 'Username and password are required' });
     }
-    
+
+    // Check if username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+
     // Create user
     const user = await User.create({ username, password });
-    res.status(200).json({ user });
+    res.status(201).json({ user });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-
 module.exports = {
   signupUser
-}
+};
