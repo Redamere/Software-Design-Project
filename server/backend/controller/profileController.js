6d30d1c21006122f9f05cb49c10dba3ee0e15cab
Andrew Dieu <45775577+andrewdieu@users.noreply.github.com>
2024-04-26 17:20:48 -0500

    Update profileController.js

--- server/backend/controller/profileController.js
+++ server/backend/controller/profileController.js
@@ -12,23 +12,34 @@ const getProfiles = async (req, res) => {
     }
 }
 
-// get a user's profile
-const getUserProfile = async (req, res) => {
-    const { id } = req.params
-    console.log(id);
+// edit a user's profile
+const editProfile = async (req, res) => {
+    console.log("here");
+    const { fullName, address1, address2, city, state, zipcode, user_id } = req.body
 
     try {
-        const profile = await Profile.findOne({ user_id: id }) // Search by user_id attribute
-        // Alternatively, you can use ES6 shorthand if the variable name matches the field name:
-        // const profile = await Profile.findOne({ user_id })
-
-        if (!profile) {
-            return res.status(404).json({ error: 'Profile not found' })
+        // Validate the address fields
+        if (address1.length > 50 || (address2 && address2.length > 50)) {
+            return res.status(400).json({ error: 'Address fields must be no longer than 50 characters' });
         }
 
-        return res.status(200).json({ profile })
+        // Validate the zipcode format
+        const zipRegex = /^\d{5}(?:[-\s]\d{4})?$/;
+        if (!zipRegex.test(zipcode)) {
+            return res.status(400).json({ error: 'Invalid zip code format' });
+        }
+
+        const profile = await Profile.findOneAndUpdate({ user_id: user_id }, {
+            fullName, address1, address2, city, state, zipcode
+        }, { new: true });
+
+        if (!profile) {
+            return res.status(404).json({ error: 'Profile not found' });
+        }
+
+        return res.status(200).json({ profile });
     } catch (error) {
-        return res.status(400).json({ error: error.message })
+        return res.status(400).json({ error: error.message });
     }
 }
 
